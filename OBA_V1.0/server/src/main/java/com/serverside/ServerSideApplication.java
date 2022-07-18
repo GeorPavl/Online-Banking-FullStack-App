15e50f468b62d6912fad53c55fa9e85c674b9235
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-07-18 19:05:21 +0300

    add spring boot project

---
+++ OBA_V1.0/server/src/main/java/com/serverside/ServerSideApplication.java
@@ -0,0 +1,15 @@
+package com.serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideApplication.class, args);
+	}
+
+}
+
+
